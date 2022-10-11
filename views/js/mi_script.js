c1a6affe9408e978e6742d5b51a3dbaf8b931974
Pau Figueras <pau@pswsm.cat>
2022-10-11 12:25:58 +0200

    Add DataTables

--- views/js/mi_script.js
+++ views/js/mi_script.js
@@ -1,106 +1,114 @@
 function obtainTables () {
-  $.ajax({
-    url: 'https://plantl.api.aina.cf/evalesapi/tables',
-    dataType: 'json',
-    // contentType: "application/json",
-    success: tableSuccess,
-    error: tableError
-  })
+	$.ajax({
+		url: 'https://plantl.api.aina.cf/evalesapi/tables',
+		dataType: 'json',
+		// contentType: "application/json",
+		success: tableSuccess,
+		error: tableError
+	})
 }
 
 function tableSuccess (d) {
-  console.log(d)
+	console.log('d:', d)
+	const arr = d.map((element) => {
+		return [
+			element.modelName,
+			element.researchGroup,
+			element.paperLink,
+			element.conll_nerc.F1,
+			element.capitel_nerc.F1,
+			element.ud_pos.F1,
+			element.capitel_pos.F1,
+			element.mldoc.F1,
+			element.paws_x.F1,
+			element.sts.Combined,
+			element.sqac.F1,
+			// /\d+\.\d{2}/.exec(element.winograd.F1),
+			element.xnli.Accuracy
+		]
+	})
 
-  const arr = d.map((element) => {
-    return [
-      element.email,
-      element.modelName,
-      element.researchGroup,
-      element.paperLink,
-      element.conll_nerc.F1,
-      element.capitel_nerc.F1,
-      element.ud_pos.F1,
-      element.capitel_pos.F1,
-      element.mldoc.F1,
-      element.paws_x.F1,
-      element.sts.Combined,
-      element.sqac.F1,
-      // /\d+\.\d{2}/.exec(element.winograd.F1),
-      element.xnli.Accuracy
-    ]
-  })
+	const headers = [
+		'Model',
+		'Submitted by',
+		'Paper',
+		'CoNLL-NERC (F1)',
+		'CAPITEL-NERC (F1)',
+		'UD-POS (F1)',
+		'CAPITEL-POS (F1)',
+		'MLDoc (F1)',
+		'PAWS-X (F1)',
+		'STS (Comb.)',
+		'SQAC (F1)',
+		// 'WINOGRAD',
+		'XNLI (Acc.)'
+	]
 
-  const headers = [
-    'Rank',
-    'Model',
-    'Group',
-    'Paper',
-    'CoNLL-NERC (F1)',
-    'CAPITEL-NERC (F1)',
-    'UD-POS (F1)',
-    'CAPITEL-POS (F1)',
-    'MLDoc (F1)',
-    'PAWS-X (F1)',
-    'STS (Comb.)',
-    'SQAC (F1)',
-    // 'WINOGRAD',
-    'XNLI (Acc.)'
-  ]
+	const idHref = [
+		'',
+		'',
+		'',
+		'nerc',
+		'nerc',
+		'pos',
+		'pos',
+		'mldoc',
+		'pawsx',
+		'sts',
+		'qa',
+		'xnli'
+	]
 
-  const idHref = [
-    '',
-    '',
-    '',
-    '',
-    'conll-nerc',
-    'capitel-nerc',
-    'ud-pos',
-    'capitel-pos',
-    'mldoc',
-    'paws-x',
-    'sts',
-    'sqac',
-    'xnli'
-  ]
+	let innerTable = '<table id="table">'
+	innerTable += '<thead><tr>'
+	$(headers).each(function (header) {
+		if (header > 2) {
+			innerTable += '<th><a href="datasets.html#' + idHref[header].toLowerCase() + '">' + headers[header] + "</a></th>"
+		} else {
+			innerTable += '<th>' + headers[header] + "</th>"
+		}
+	})
+	innerTable += '</tr></thead><tbody>'
 
-  let innerTable = '<table class="table performanceTable">'
-  innerTable += '<tr>'
-  $(headers).each(function (header) {
-    if (header > 3) {
-      innerTable += '<th><a href="datasets.html#' + idHref[header].toLowerCase() + '">' + headers[header]
-    } else {
-      innerTable += '<th>' + headers[header]
-    }
-  })
+	$(arr).each(function (elem) {
+		innerTable += '<tr>'
+		$(arr[elem]).each(function (innerElem) {
+			switch (innerElem) {
+				// case 0:
+				//	innerTable += '<td>' + (elem + 1)
+				//	break
+				case 2:
+					if (arr[elem][innerElem] != '') {
+						innerTable += '<td><a target="_blank" href=' + arr[elem][innerElem] + '><span class="material-symbols-outlined">open_in_new</span></a></td>'
+					} else {
+						innerTable += '<td></td>'
+					}
+					break
+				default:
+					innerTable += '<td>' + arr[elem][innerElem] + "</td>"
+			}
+		})
+		innerTable += "</tr>"
+	})
+	innerTable += "</tbody></table>"
 
-  $(arr).each(function (elem) {
-    innerTable += '<tr>'
-    $(arr[elem]).each(function (innerElem) {
-      switch (innerElem) {
-        case 0:
-          innerTable += '<td>' + (elem + 1)
-          break
-        case 3:
-          innerTable += '<td><a target="_blank" href=' + arr[elem][innerElem] + '><span class="material-symbols-outlined">open_in_new'
-          break
-        case 2:
-          innerTable += '<td>' + arr[elem][innerElem] // + '<br>' + arr[elem][0]
-          break
-        default:
-          innerTable += '<td>' + arr[elem][innerElem]
-      }
-    })
-  })
-
-  $('#leaderboard').html(innerTable)
+	$('#leaderboard').html(innerTable)
+	$('#table').DataTable({
+		paging: false,
+		order: [[3, 'desc'], [4, 'desc'], [5, 'desc'], [6, 'desc'], [7, 'desc'], [8, 'desc'], [9, 'desc'], [10, 'desc'], [11, 'desc']],
+		columnDefs: [
+			{ targets: [0, 1, 2], orderable: false }
+		],
+		searching: false
+	});
 }
 
 function tableError (e) {
-  console.log(e)
+	console.log(e)
 }
 
 $(document).ready(() => {
-  obtainTables()
+	obtainTables()
 
-  // console.log(arr)
+	// console.log(arr)
 })
