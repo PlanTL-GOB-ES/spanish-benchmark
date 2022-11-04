78b2cfc3b3eb18d41a7aeb036cd762b13c2dbaf9
PaulNdrei <andreipetrea3@gmail.com>
2022-11-04 16:52:49 +0100

    Change form API URL

--- views/js/form.js
+++ views/js/form.js
@@ -59,8 +59,7 @@ function submitForm (e) {
   if (!$('#evaluation_form div').hasClass('has-error') && $("#dataPol").is(":checked")) {
     $('#submit_button').val('Submit')
     $.ajax({
-      // url: 'https://bsclsaina01.bsc.es/clubapi/results',
-      url: 'http://localhost:3000/api/results',
+      url: 'https://bscplantl01.bsc.es/evales/api/results',
       type: 'POST',
       data: formData,
       processData: false,
