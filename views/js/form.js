79cdb94e89ce7237f3f9e9d0f9685e92c55a3e00
Pau Figueras <pau@pswsm.cat>
2022-11-16 13:44:08 +0100

    Finish rework of form validation.

--- views/js/form.js
+++ views/js/form.js
@@ -1,52 +1,93 @@
+// form.js
+// Handles the validation and the form of https://benchmark.plantl.bsc.es/submit.html
+//
+// Made by: Pau Figueras Pavón
+// github: https://github.com/pswsm
+
+// Disables submit button by default
 $('#submit_button').attr('disabled', true)
 
+// Check if mail is valid
 function checkMail () {
-  if (/[\w-]+@\w+\.\w{2,3}/.test($(this).value)) {
-    $('#emailDiv').removeClass('has-error');
-    $('#emailDiv > div.help-block').remove();
-		return true;
+  if (/[\w-]+@\w+\.\w{2,3}/.test($(this).get(0).value)) {
+    $(this).parent().removeClass('has-error').children('div.help-block').remove();
+		// return true;
   } else if ($('#emailDiv > div.help-block').length === 0) {
-		$('#emailDiv').addClass('has-error').append('<div class="help-block">Mail ' + $(this).value + ' is not valid</div>');
-		return false;
+		$('#emailDiv').addClass('has-error').append('<div class="help-block">Mail ' + $(this).get(0).value + ' is not valid</div>');
+		// return false;
 	}
 }
 
+
+// Add as blur event
 $('#email').blur(checkMail)
 
+// Check if file fiedl is valid (has a file uploaded)
 function checkFile () {
   if ($(this).get(0).files.length !== 0) {
-    $('#' + $(this).attr('id')).parent().removeClass('has-error')
-		console.log('this:', this)
-    $(this).parent().children('div.help-block').remove()
-		return true
+    $(this).parent().removeClass('has-error').children('div.help-block').remove()
+		// return true
 	} else if ($(this).parent().children('div.help-block').length === 0) {
 		$(this).parent().addClass('has-error').append('<div class="help-block">File is not valid</div>')
-		return false;
+		// return false;
   }
 }
 
+// Add as blur event
 $("input[type=file]").blur(checkFile)
 
+// Check text fields not empty
 function checkText () {
-  if ($(this).value !== undefined || $(this).value !== '') {
-    $(this).parent().removeClass('has-error')
-    $(this).parent().children('div.help-block').remove()
-		return true;
-	} else if ($(this).parent().children('div.help-block').length === 0){
-		$(this).addClass('has-error').children('div.help-block').text('Name is not valid')
-		return false;
-  }
-}
-
-function checkLink () {
-	if ($(this).value === undefined || $(this).value === '' || /(?:(?:http|https):\/\/)?(?:www\.)?\w+(?:\.\w{2,3})/.test($(this).value)) {
-		return true
-	} else if ($(this).parent().children('div.help-block').length === 0) {
-		$(this).parent().append('<div class="help=block">URL not valid</div>')
-		return false
+  if ($(this).get(0).value == undefined || $(this).get(0).value == '') {
+		if ($(this).parent().children('div.help-block').length === 0){
+			$(this).parent().addClass('has-error').append('<div class="help-block">Field not valid not valid</div>')
+		}
+	} else {
+    $(this).parent().removeClass('has-error').children('div.help-block').remove()
+    // $(this).parent().children('div.help-block').remove()
 	}
 }
 
+// Add as blur event
+$('input[type=text]').blur(checkText)
+
+
+// Check if URL field is either empty, since it's optional or it's value looks like an url
+function checkLink () {
+	// console.log('$(this).get(0).value:', $(this).get(0).value);
+	if ($(this).get(0).value === '' || /(?:(?:http|https):\/\/)?(?:www\.)?\w+(?:\.\w{2,3})/.test($(this).get(0).value)) {
+    $(this).parent().removeClass('has-error').children('div.help-block').remove()
+	} else {
+		if ($(this).parent().children('div.help-block').length === 0) {
+			$(this).parent().addClass('has-error').append('<div class="help-block">URL not valid not valid</div>')
+		}
+	}
+}
+
+// Add as blur event
+$('input[type=url]').blur(checkLink)
+
+// On form submit
+function submitForm (e) {
+	$('#submit_button').val('Submit')
+	$('#evaluation_form').css('display', 'none').parent().append('<img src="../ok.png" alt="Evaluation sent successfully">')
+	$('#evaluation_form + img').css('filter', 'invert(100%)').css('text-align', 'center')
+	$('#evaluation_form + p').css('display', 'none')
+	$('#evaluation_form + h1').text('Thanks for submitting!')
+  const formData = new FormData($('#evaluation_form')[0])
+	$.ajax({
+		// url: 'localhost:3000/api/results',
+		url: 'https://bscplantl01.bsc.es/evales/api/results',
+		type: 'POST',
+		data: formData,
+		processData: false,
+		contentType: false,
+		success: submitSuccess,
+		error: submitError
+	})
+}
+
+// On hover submit button, validate all fields too
 $("#submit_button").hover(function () {
 	$("input[type=text]").each(checkText)
 	$("input[type=file]").each(checkFile)
@@ -58,41 +99,12 @@ $("#submit_button").hover(function () {
 	let mailOk = $("input[type=email]").parent().hasClass('has-error')
 	let legalOk = $("#dataPol").is(":checked")
 	console.log('textsOk, filesOk, urlOk, mailOk, legalOk:', textsOk, filesOk, urlOk, mailOk, legalOk)
-	if (filesOk && textsOk && urlOk && mailOk && legalOk) {
-		$('#submit_button').attr('disabled', false)
+	if (!(filesOk && textsOk && urlOk && mailOk) && legalOk) {
+		$('#submit_button').attr('disabled', false).click(submitForm)
 	}
 }, function () {})
 
-function submitForm (e) {
-  const formData = new FormData($('#evaluation_form')[0])
-  // let mailValid = checkMail(formData.get('email'))
-  // let linkValid = checkLink(formData.get('paperLink'))
-  if (!$('#submit_button').is(':disabled')) {
-    $('#submit_button').val('Submit')
-    $.ajax({
-			url: 'localhost:3000',
-      // url: 'https://bscplantl01.bsc.es/evales/api/results',
-      type: 'POST',
-      data: formData,
-      processData: false,
-      contentType: false,
-      success: submitSuccess,
-      error: submitError
-    })
-  } else {
-    $('#evaluation_form div').each(function () {
-      if ($(this).hasClass('has-error') && $(this).children('div.help-block').length === 0) {
-        $(this).append('<div class="help-block">Field not valid</div>')
-      }
-    })
-  }
-}
-
 function submitSuccess () {
-  $('#evaluation_form').css('display', 'none').parent().append('<img src="../ok.png" alt="Evaluation sent successfully">')
-  $('#evaluation_form + img').css('filter', 'invert(100%)').css('text-align', 'center')
-  $('#evaluation_form + p').css('display', 'none')
-  $('#evaluation_form + h1').text('Thanks for submitting!')
 }
 
 function submitError (err) {
