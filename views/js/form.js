1e39f1430f572463270138cb54589d4380046151
Pau Figueras <pau@pswsm.cat>
2022-12-01 12:08:51 +0100

    Fix form on chrome

--- views/js/form.js
+++ views/js/form.js
@@ -4,60 +4,48 @@
 // Made by: Pau Figueras Pavón
 // github: https://github.com/pswsm
 
-// Disables submit button by default
-$('#submit_button').attr('disabled', true)
-let onClickAlready = false;
+// Global var to check if an evaluation has been sent
+let evaluationSent = false;
 
 // Check if mail is valid
 function checkMail () {
-  if (/[\w-]+@\w+\.\w{2,3}/.test($(this).get(0).value)) {
-    $(this).parent().removeClass('has-error').children('div.help-block').remove();
+	if (/[\w-]+@\w+\.\w{2,3}/.test($(this).get(0).value)) {
+		$(this).parent().removeClass('has-error').children('div.help-block').remove();
 		// return true;
-  } else if ($('#emailDiv > div.help-block').length === 0) {
+	} else if ($('#emailDiv > div.help-block').length === 0) {
 		$('#emailDiv').addClass('has-error').append('<div class="help-block">Mail ' + $(this).get(0).value + ' is not valid</div>');
 		// return false;
 	}
 }
 
-
-// Add as blur event
-$('#email').blur(checkMail)
-
-// Check if file fiedl is valid (has a file uploaded)
-function checkFile () {
-  if ($(this).get(0).files.length !== 0) {
-    $(this).parent().removeClass('has-error').children('div.help-block').remove()
-		// return true
-	} else if ($(this).parent().children('div.help-block').length === 0) {
-		$(this).parent().addClass('has-error').append('<div class="help-block">File is not valid</div>')
-		// return false;
-  }
-}
-
-// Add as blur event
-$("input[type=file]").blur(checkFile)
-
 // Check text fields not empty
 function checkText () {
-  if ($(this).get(0).value == undefined || $(this).get(0).value == '') {
+	if ($(this).get(0).value == undefined || $(this).get(0).value == '') {
 		if ($(this).parent().children('div.help-block').length === 0){
 			$(this).parent().addClass('has-error').append('<div class="help-block">Field not valid</div>')
 		}
 	} else {
-    $(this).parent().removeClass('has-error').children('div.help-block').remove()
-    // $(this).parent().children('div.help-block').remove()
+		$(this).parent().removeClass('has-error').children('div.help-block').remove()
+		// $(this).parent().children('div.help-block').remove()
 	}
 }
 
-// Add as blur event
-$('input[type=text]').blur(checkText)
-
+// Check if file field is valid (has a file uploaded)
+function checkFile () {
+	if ($(this).get(0).files.length !== 0) {
+		$(this).parent().removeClass('has-error').children('div.help-block').remove()
+		// return true
+	} else if ($(this).parent().children('div.help-block').length === 0) {
+		$(this).parent().addClass('has-error').append('<div class="help-block">File is not valid</div>')
+		// return false;
+	}
+}
 
 // Check if URL field is either empty, since it's optional or it's value looks like an url
 function checkLink () {
 	// console.log('$(this).get(0).value:', $(this).get(0).value);
 	if ($(this).get(0).value === '' || /(?:(?:http|https):\/\/)?(?:www\.)?\w+(?:\.\w{2,3})/.test($(this).get(0).value)) {
-    $(this).parent().removeClass('has-error').children('div.help-block').remove()
+		$(this).parent().removeClass('has-error').children('div.help-block').remove()
 	} else {
 		if ($(this).parent().children('div.help-block').length === 0) {
 			$(this).parent().addClass('has-error').append('<div class="help-block">URL not valid not valid</div>')
@@ -65,15 +53,13 @@ function checkLink () {
 	}
 }
 
-// Add as blur event
-$('input[type=url]').blur(checkLink)
-
 // On form submit
 function submitForm (e) {
-  const formData = new FormData($('#evaluation_form')[0])
+	evaluationSent = true;
+	const formData = new FormData($('#evaluation_form')[0]);
+	// Disable button
 	$('#submit_button').val('Submit').attr('disabled', true)
 	// $('#evaluation_form + img').css('filter', 'invert(100%)').css('text-align', 'center')
-	// Disable button
 	// Toast evaluating...
 	Toastify({
 		text: "Evaluating...",
@@ -84,7 +70,7 @@ function submitForm (e) {
 		}
 	}).showToast()
 	$.ajax({
-		// url: 'http://localhost:3000/api/results',
+		//url: 'http://localhost:3000/api/results',
 		url: 'https://bscplantl01.bsc.es/evales/api/results',
 		type: 'POST',
 		data: formData,
@@ -100,20 +86,21 @@ function submitSuccess () {
 	$('#evaluation_form').parent().empty().append('<h1>Thanks for submitting!</h1><br><img src="./images/ok.png" alt="Evaluation sent successfully">')
 
 	console.log('Upload okay')
+	evaluationSent = false;
 }
 
 function submitError (err) {
-  console.error(err)
-  // Toast error code + detail.
-  // Enable button
+	console.error(err)
+	// Toast error code + detail.
+	// Enable button
 	switch (err.status) {
-		case 500:
+		case 400:
 			let responseParsed = JSON.parse(err.responseText)
 			let failedTasks = responseParsed.evaluations_error.join(', ')
 			toast = Toastify({
 				text: failedTasks + " failed, please check the files are the correct ones",
 				duration: 3000,
-				style: { background: "#ff0000" }
+				style: { background: "#f00" }
 			})
 			break;
 
@@ -127,25 +114,54 @@ function submitError (err) {
 	}
 	toast.showToast();
 	$('#submit_button').attr('disabled', true);
+	evaluationSent = false;
 }
 
-// On hover submit button, validate all fields too
-$("#submit_button").hover(function () {
-	$("input[type=text]").each(checkText)
-	$("input[type=file]").each(checkFile)
-	$("input[type=url]").each(checkLink)
-	$("input[type=email]").each(checkMail)
-	let textsOk = $("input[type=text]").parent().hasClass('has-error') 
-	let filesOk = $("input[type=files]").parent().hasClass('has-error')
-	let urlOk = $("input[type=url]").parent().hasClass('has-error')
-	let mailOk = $("input[type=email]").parent().hasClass('has-error')
-	let legalOk = $("#dataPol").is(":checked")
-	// console.log('textsOk, filesOk, urlOk, mailOk, legalOk:', textsOk, filesOk, urlOk, mailOk, legalOk)
-	if (!(filesOk && textsOk && urlOk && mailOk) && legalOk) {
-		$('#submit_button').attr('disabled', false)
-		if (!onClickAlready) {
-			$('#submit_button').click(submitForm);
-			onClickAlready = true;
+$(document).ready(function () {
+	console.log('okay');
+	// Disables submit button by default
+	$('#submit_button').attr('disabled', true)
+	let onClickAlready = false;
+
+	// Add as blur event
+	$('#email').blur(checkMail)
+	console.log('blur mail');
+
+	// Add as blur event
+	$("input[type=file]").blur(checkFile)
+	console.log('blur files');
+
+	// Add as blur event
+	$('input[type=text]').blur(checkText)
+	console.log('blur text');
+
+	// Add as blur event
+	$('input[type=url]').blur(checkLink)
+	console.log('blur url');
+
+	// On hover submit button, validate all fields too
+	console.log('pre focus')
+	$("#submit_button").on('pointerenter', function () {
+		console.log('in focus')
+		$("input[type=text]").each(checkText)
+		$("input[type=file]").each(checkFile)
+		$("input[type=url]").each(checkLink)
+		$("input[type=email]").each(checkMail)
+		let textsOk = !$("input[type=text]").parent().hasClass('has-error') 
+		let filesOk = !$("input[type=files]").parent().hasClass('has-error')
+		let urlOk = !$("input[type=url]").parent().hasClass('has-error')
+		let mailOk = !$("input[type=email]").parent().hasClass('has-error')
+		let legalOk = $("#dataPol").is(":checked")
+		// console.log('textsOk, filesOk, urlOk, mailOk, legalOk:', textsOk, filesOk, urlOk, mailOk, legalOk)
+		if (!evaluationSent && filesOk && textsOk && urlOk && mailOk && legalOk) {
+			$('#submit_button').attr('disabled', false)
+			if (!onClickAlready) {
+				$('#submit_button').click(submitForm);
+				onClickAlready = true;
+			}
+		} else {
+			$('#submit_button').attr('disabled', true)
 		}
-	}
-}, function () {})
+	})
+})
+
